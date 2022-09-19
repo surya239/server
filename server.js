d59933228d105381b4bde09a9be87ee56b326e4d
Surya <suryab.2392000@gmail.com>
2022-09-19 22:48:17 +0530

    hello

--- server.js
+++ server.js
@@ -1,13 +1,18 @@
 import { createRequire } from "module";
 const require = createRequire(import.meta.url);
+const path = require('path')
+
 const express = require("express");
 const app = express();
 const jwt = require('jsonwebtoken')
 const cors = require('cors');
 app.use(cors());
 app.use(express.json())
+const __dirname = path.resolve();
+const html = "<h1> Hello World </h1>"
 app.get("*", (req, res) => {
-    res.send("Hello")
+
+    res.send(html)
 } )
 const PORT = 5000
 app.listen(PORT, () =>{
